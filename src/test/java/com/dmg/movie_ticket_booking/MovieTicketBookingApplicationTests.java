06252bf2e0eb311feb61dc3cae9678cff5827150
N Santhosh Kumar <nsanthoshkumar1508@gmail.com>
2026-09-18 22:14:30 +0530

    initialization

---
+++ src/test/java/com/dmg/movie_ticket_booking/MovieTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmg.movie_ticket_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
